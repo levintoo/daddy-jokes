318467146f9f398c4fc7fe9429cef0ae63ef9d55
levintoo <tooklevin@gmail.com>
2021-07-08 15:27:14 +0300

    all in one responsiveness

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const jokeEl= document.getElementById('joke');
 const get_joke = document.getElementById('get-joke');
 
- get_joke.addEventListener('click',);
+ get_joke.addEventListener('click', generateJoke());
 
- generateJoke();
+ 
 
 async function generateJoke() {
     const jokeRes= await  fetch('https://icanhazdadjoke.com/',{
