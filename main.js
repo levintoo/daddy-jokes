1c3063468c741334281acaad127a7291b4daa660
levintoo <tooklevin@gmail.com>
2021-07-08 15:15:15 +0300

    fetch joke

---
+++ main.js
@@ -0,0 +1,17 @@
+const jokeEl= document.getElementById('joke');
+const get_joke = document.getElementById('get-joke');
+
+ get_joke.addEventListener('click',);
+
+ generateJoke();
+
+async function generateJoke() {
+    const jokeRes= await  fetch('https://icanhazdadjoke.com/',{
+    headers:  {
+        'Accept': 'application/json'
+    }
+    });
+    const joke = await jokeRes.json();
+
+    console.log(joke);
+}
